b863ecd2d251d6b692620536f15d12cec7ac03aa
Arthur Nicholson-Gumuła <arthur@cloudawesome.uk>
2020-12-24 18:06:23 +0000

    Basic implementation for creation and updates of entities; Merry Christmas =D

--- src/CloudAwesome.Xrm.Customisation/CloudAwesome.Xrm.Customisation/ConfigurationWrapper.cs
+++ src/CloudAwesome.Xrm.Customisation/CloudAwesome.Xrm.Customisation/ConfigurationWrapper.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections.Generic;
+using CloudAwesome.Xrm.Core;
 using Microsoft.Extensions.Logging;
 using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
 
 namespace CloudAwesome.Xrm.Customisation
 {
@@ -16,8 +18,53 @@ namespace CloudAwesome.Xrm.Customisation
 
         public void GenerateCustomisations(ConfigurationManifest manifest, IOrganizationService client, ILogger logger)
         {
+            var t = new TracingHelper(logger);
+            t.Debug($"Entering ConfigurationWrapper.GenerateCustomisations");
 
+            var publisherPrefix = GetPublisherPrefixFromSolution(client, manifest.SolutionName);
+            t.Debug($"Publisher prefix retrieved: {publisherPrefix}");
 
+            foreach (var entity in manifest.Entities)
+            {
+                // BUG - *** There's an error somewhere here where only the first two entities are being processed and the others in the manifest aren't.
+                // Pick it up here after Chrimbo - Wesołych Świąt ;)
+
+                t.Debug($"Processing entity: {entity.SchemaName}");
+                entity.CreateOrUpdate(client, publisherPrefix, manifest);
+                t.Info($"Entity {entity.SchemaName} successfully processed");
+
+                if (entity.Attributes == null) return;
+                foreach (var attribute in entity.Attributes)
+                {
+                    
+                }
+            }
         }
+
+        public string GetPublisherPrefixFromSolution(IOrganizationService client, string solutionName)
+        {
+            var fetchQuery = SolutionPublisherQuery.Replace("{{SolutionName}}", solutionName);
+            var publisher = 
+                QueryExtensions.RetrieveRecordFromQuery(client, new FetchExpression(fetchQuery))
+                    .ToEntity<Publisher>();
+
+            return publisher.CustomizationPrefix;
+        }
+
+        private const string SolutionPublisherQuery =
+            @"<fetch version=""1.0"" output-format=""xml-platform"" mapping=""logical"" distinct=""true"">
+	            <entity name=""publisher"">
+		            <attribute name=""publisherid""/>
+		            <attribute name=""friendlyname""/>
+		            <attribute name=""uniquename""/>
+		            <attribute name=""customizationprefix""/>
+		            <link-entity name=""solution"" from=""publisherid"" to=""publisherid"" link-type=""inner"" alias=""p"">
+			            <filter type=""and"">
+				            <condition attribute=""uniquename"" operator=""eq"" value=""{{SolutionName}}""/>
+			            </filter>
+		            </link-entity>
+	            </entity>
+            </fetch>";
     }
+
 }
---
+++ src/CloudAwesome.Xrm.Customisation/CloudAwesome.Xrm.Customisation/Exceptions/NotCustomisableException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CloudAwesome.Xrm.Customisation.Exceptions
+{
+    public class NotCustomisableException: Exception
+    {
+        public NotCustomisableException()
+        {
+
+        }
+
+        public NotCustomisableException(string message) : base(message)
+        {
+
+        }
+
+        public NotCustomisableException(string message, Exception innerException) : base(message, innerException)
+        {
+
+        }
+    }
+}
